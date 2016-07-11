ab35e6b0d9ca93bc0036d62d38c5a2519e5f4635
zainnadeem <zn.nadeem@gmail.com>
2016-07-11 14:21:03 -0400

    con Init

---
+++ swift-boat/boat.swift
@@ -0,0 +1,29 @@
+//
+//  boat.swift
+//  swift-boat
+//
+//  Created by Zain Nadeem on 7/11/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Boat {
+    
+    let name: String
+    var sailors: [String]
+    var maxSpeedKnots: Double
+    var speedKnots: Double = 0
+    
+    init(name: String, sailors: [String], maxSpeedKnots:Double) {
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+    }
+    
+    convenience init(name:String, maxSpeedKnots:Double){
+        self.init(name: name, sailors: [], maxSpeedKnots: maxSpeedKnots)
+    }
+    
+    
+}
